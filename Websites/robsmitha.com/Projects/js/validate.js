58747b0ca35ca75be60aab9352a0ebb9aaa389cc
Rob Smitha <robsmitha@outlook.com>
2017-04-23 10:49:57 -0400

    pre gym and split up modules

--- Websites/robsmitha.com/Projects/js/validate.js
+++
@@ -1,9 +0,0 @@
-function validate() {
-    var firstname = document.forms["RegForm"]["firstname"].value;
-    if (firstname == "")
-    {
-        document.getElementById("#fname_err").innerHTML = "First name cannnot be blank";
-    }
-    document.getElementById("#fname_err").innerHTML = "First name cannnot be blank";
-    alert();
-}
